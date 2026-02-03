de9328b995f5a489dac23a561224b54a93313885
maxmineew <136315097+maxmineew@users.noreply.github.com>
2026-02-03 20:10:59 +0300

    Update Задача 1. Машина3.py
    Вам предстоит снова немного видоизменить класс Toyota из прошлого урока. На всякий случай вот описание класса.
    Четыре атрибута:
    1.	цвет машины (например, красный),
    2.	цена (один миллион),
    3.	максимальная скорость (200),
    4.	текущая скорость (ноль).
    Два метода:
    1.	Отображение информации об объекте класса.
    2.	Метод, который позволяет устанавливать текущую скорость машины.
    Теперь все четыре атрибута должны инициализироваться при создании экземпляра класса (то есть передаваться в init). Реализуйте такое изменение класса.

--- 11_Модуль_24/11.4 Конструктор __init__ и работа с несколькими классами/Задача 1. Машина3.py
+++ 11_Модуль_24/11.4 Конструктор __init__ и работа с несколькими классами/Задача 1. Машина3.py
@@ -1,1 +1,17 @@
+class Toyota:# это класс (шаблон, чертёж всех машин этой марки)
 
+    def __init__(self, colour = 'red', price = 1e6, max_speed = 200, current_speed = 0):
+        self.colour = colour
+        self.price = price
+        self.max_speed = max_speed
+        self.current_speed = current_speed
+
+    def check_info(self):
+        print(self.colour, self.price, self.max_speed, self.current_speed)
+    def change_speed(self, new_speed):
+        self.current_speed = new_speed
+
+my_car = Toyota()  # создаем экземпляр
+my_car.check_info()  # выводим информацию
+my_car.change_speed(2)  # меняем скорость
+my_car.check_info()  # снова выводим информацию
